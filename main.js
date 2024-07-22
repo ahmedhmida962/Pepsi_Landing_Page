2cdca95fdc009073bc47d1dbf2467fb1e89de7cf
Ahmed  BOUDKHIL <90070683+ahmedhmida962@users.noreply.github.com>
2024-07-22 16:24:49 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+function imageSlider(canette){
+    document.querySelector(".pepsi").src = canette;
+
+}
+
+function changeBgColor(color){
+    let sec = document.querySelector(".sec");
+    sec.style.backgroundColor = color;
+}
